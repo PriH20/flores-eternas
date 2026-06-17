9c612cd56da73325fd847616154e324a1affb14c
PriH20 <ivansuky20@hotmail.com>
2026-06-17 08:33:35 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
 function pedirProducto(producto) {
-  function enviarPedido() {
-  const nombre = document.getElementById("nombre").value;
-  const producto = document.getElementById("producto").value;
-  const mensajeTarjeta = document.getElementById("mensaje").value;
-
-  const telefono = "524771429907";
-
-  const mensaje =
-    "Hola, quiero hacer un pedido.%0A%0A" +
-    "Nombre: " + nombre + "%0A" +
-    "Producto: " + producto + "%0A" +
-    "Mensaje para tarjeta: " + mensajeTarjeta;
-
-  window.location.href =
-    "https://wa.me/" + telefono + "?text=" + mensaje;
-}
-
   const telefono = "524771429907";
   const mensaje = "Hola, quiero pedir: " + producto;
   const url = "https://wa.me/" + telefono + "?text=" + encodeURIComponent(mensaje);
 
   window.location.href = url;
 }
+
 function enviarPedido() {
   const nombre = document.getElementById("nombre").value;
   const producto = document.getElementById("producto").value;
@@ -30,13 +14,34 @@ function enviarPedido() {
   const telefono = "524771429907";
 
   const mensaje =
-    `Hola, quiero hacer un pedido.%0A%0A` +
-    `Nombre: ${nombre}%0A` +
-    `Producto: ${producto}%0A` +
-    `Mensaje para tarjeta: ${mensajeTarjeta}`;
+    "Hola, quiero hacer un pedido.\n\n" +
+    "Nombre: " + nombre + "\n" +
+    "Producto: " + producto + "\n" +
+    "Mensaje para tarjeta: " + mensajeTarjeta;
 
-  window.open(
-    `https://wa.me/${telefono}?text=${mensaje}`,
-    "_blank"
-  );
+  const url = "https://wa.me/" + telefono + "?text=" + encodeURIComponent(mensaje);
+
+  window.location.href = url;
+}
+
+function mostrarCatalogo() {
+  document.getElementById("contenido").classList.remove("oculto");
+
+  setTimeout(function () {
+    document.getElementById("catalogo").scrollIntoView({
+      behavior: "smooth"
+    });
+  }, 100);
+}
+function toggleMenu() {
+  document.getElementById("menu").classList.toggle("oculto");
+}
+let carrito = [];
+
+function agregarCarrito(producto, precio) {
+  carrito.push({ producto, precio });
+
+  document.getElementById("cartCount").textContent = carrito.length;
+
+  alert("Producto agregado al carrito 🛒");
 }
